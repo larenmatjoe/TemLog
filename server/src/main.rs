cc7bf109437357639dec1637e42b2459fe15724c
larenmatjoe <jooeel777@gmail.com>
2024-04-25 15:16:41 +0530

    Create variables

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,4 +2,5 @@ use std::net::{TcpListener, TcpStream};
 //use std::thread;
 fn main() {
     let _listener = TcpListener::bind("127.0.0.1:9000").unwrap();
+    let _connection = TcpStream::connect("127.0.0.1:9000").unwrap();
 }
