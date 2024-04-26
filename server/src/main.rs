1463c9fb5000a22f8fa94e6f27ea2727675348d9
larenmatjoe <jooeel777@gmail.com>
2024-04-26 21:57:39 +0530

    Connection Setup

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,17 +1,34 @@
-//use std::net::TcpListener, TcpStream};
-use std::net::TcpListener;
-//use std::thread;
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+use std::io::prelude::*;
+fn handling(stream :Result<TcpStream, std::io::Error>) {
+    match stream {
+        Ok(mut stream) => {
+            let ip = stream.peer_addr().unwrap();
+            println!("New Client: {}",ip);
+            let _ = stream.write(b"Connected to Server!");
+            loop {
+                let mut buffer = [0;65535];
+                let _ = stream.read(&mut buffer);
+                if buffer[0] == b'\0' {
+                    break;
+                }
+                if buffer[0] == b'\n' {
+                    continue;
+                }
+                let data = &String::from_utf8_lossy(&buffer)[0..3];
+                //println!("Received : {} bytes",data.chars());
+                println!("Payload : {}",data);
+            }
+        }
+        Err(e) => {
+            println!("Error : {}",e);
+        }
+    }
+}
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:9000").unwrap();
     for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                let ip = stream.peer_addr().unwrap();
-                println!("New Client: {}",ip);
-            }
-            Err(e) => {
-                println!("Stream Error : {}",e);
-            }
-        }
+        let _ = thread::spawn(move || {handling(stream)});
     }
 }
