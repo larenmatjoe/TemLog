d5fc3e87e088f16893eb90a74e3062a816e3d28e
larenmatjoe <jooeel777@gmail.com>
2024-04-26 20:31:20 +0530

    Update main

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,6 +1,15 @@
 use std::net::{TcpListener, TcpStream};
 //use std::thread;
 fn main() {
-    let _listener = TcpListener::bind("127.0.0.1:9000").unwrap();
-    let _connection = TcpStream::connect("127.0.0.1:9000").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:9000").unwrap();
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+            
+            }
+            Err(e) => {
+                println!("Stream Error : {}",e);
+            }
+        }
+    }
 }
