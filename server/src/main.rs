7d132830c3a0891cc880522b125c917fb0637391
larenmatjoe <jooeel777@gmail.com>
2024-04-26 20:35:50 +0530

    Small Update

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,11 +1,13 @@
-use std::net::{TcpListener, TcpStream};
+//use std::net::TcpListener, TcpStream};
+use std::net::TcpListener;
 //use std::thread;
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:9000").unwrap();
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-            
+                let ip = stream.peer_addr().unwrap();
+                println!("New Client: {}",ip);
             }
             Err(e) => {
                 println!("Stream Error : {}",e);
